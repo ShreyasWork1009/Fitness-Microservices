193f2d9b44591fcabf598ae29e6ad32d87327ac0
ShreyasWork1009 <shreyas.work1009@gmail.com>
2026-02-11 21:44:34 +0530

    Update AiServiceApplication.java
    cli testing 3

--- AIService/src/main/java/com/example/AIService/AiServiceApplication.java
+++ AIService/src/main/java/com/example/AIService/AiServiceApplication.java
@@ -11,6 +11,7 @@ public class AiServiceApplication {
         SpringApplication.run(AiServiceApplication.class, args);
         System.out.println("Hello World");
 	   System.out.println("Hello World1");
+		System.out.println("Hello World2");
 	}
 
 }
