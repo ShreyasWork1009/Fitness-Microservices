ca33de03b8122361fff2c29e7eeb6816cd583828
ShreyasWork1009 <shreyas.work1009@gmail.com>
2026-02-11 21:32:13 +0530

    testing cli

--- AIService/src/main/java/com/example/AIService/AiServiceApplication.java
+++ AIService/src/main/java/com/example/AIService/AiServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiServiceApplication.class, args);
+
+        SpringApplication.run(AiServiceApplication.class, args);
+        System.out.println("Hello World");
 	}
 
 }
